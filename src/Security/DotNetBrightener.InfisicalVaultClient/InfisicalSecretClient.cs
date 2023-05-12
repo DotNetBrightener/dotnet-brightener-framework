df6c906c9257ba26c7dc726d1f41532c86014414
Justin Nguyen <justinchasez04121990@gmail.com>
2023-05-13 00:14:51 +0700

    Removed console log

--- src/Security/DotNetBrightener.InfisicalVaultClient/InfisicalSecretClient.cs
+++ src/Security/DotNetBrightener.InfisicalVaultClient/InfisicalSecretClient.cs
@@ -157,8 +157,6 @@ public class InfisicalSecretClient
             var responseStream = await response.Content.ReadAsStringAsync();
             var data           = JsonConvert.DeserializeObject<TResult>(responseStream);
 
-            Console.WriteLine($"Data received: {responseStream}");
-
             return data;
         }
 
