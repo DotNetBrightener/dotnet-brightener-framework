fbec75a2b28b087baea7f1338caec3c63e33ff8e
Justin Nguyen <justinchasez04121990@gmail.com>
2024-02-02 19:39:20 +0700

    WIP: gRPC Demo App (Commit for Rolling Back)

---
+++ demos & tests/CRUD_With_gRPC_Demo/gRPCWebDemo/GrpcServiceProvider.cs
@@ -0,0 +1,12 @@
+﻿using CRUD_With_gRPC.Core;
+using DotNetBrightener.gRPC;
+
+namespace gRPCWebDemo;
+
+public class GrpcServiceProvider : IGrpcServiceProvider
+{
+    public List<Type> ServiceTypes { get; } =
+    [
+        typeof(IProductGrpcService)
+    ];
+}
