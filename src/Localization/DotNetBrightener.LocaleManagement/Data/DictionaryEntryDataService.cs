95195bc06950df21b8e995c3a3271fba95bca712
Justin Nguyen <me@justinchasez.com>
2025-07-24 15:19:15 +0700

    WIP Fixing build

--- src/Localization/DotNetBrightener.LocaleManagement/Data/DictionaryEntryDataService.cs
+++ src/Localization/DotNetBrightener.LocaleManagement/Data/DictionaryEntryDataService.cs
@@ -1,3 +1,18 @@
+/*
+ * This is a user-customizable partial class file for DictionaryEntryDataService.
+ *
+ * You can extend and customize the auto-generated functionality by:
+ * - Overriding virtual/partial methods
+ * - Adding custom properties and methods
+ * - Implementing additional interfaces
+ * - Adding custom attributes and configurations
+ *
+ * The core generated logic is in the corresponding *.g.cs file which is
+ * embedded in the compilation and protected from modification.
+ *
+ * Entity: DictionaryEntry
+ * Generated: 2025-07-24 15:18:37
+ */
 
 using DotNetBrightener.DataAccess.Services;
 using Microsoft.Extensions.Logging;
@@ -5,14 +20,6 @@ using Microsoft.Extensions.Logging;
 using LocaleManagement.Entities;
 
 namespace LocaleManagement.Data;
-
-/// <summary>
-///     Provides the data access methods for <see cref="DictionaryEntry" /> entity.
-/// </summary>
-public partial interface IDictionaryEntryDataService 
-{
-    // Provide your custom methods here
-}
 
 public partial class DictionaryEntryDataService
 {
